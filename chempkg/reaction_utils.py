c158f6163ba76f5950ade07158b188fec2e515a8
lumy10 <bouabidnour02@gmail.com>
2025-12-11 20:49:08 +0100

    edit kinetic decomp

--- chempkg/reaction_utils.py
+++ chempkg/reaction_utils.py
@@ -1,5 +1,6 @@
-import atom
 import mol
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def valid_reaction(reactives: list[tuple[mol.Molecule, int]],products: list[tuple[mol.Molecule, int]]):
@@ -17,3 +18,20 @@ def valid_reaction(reactives: list[tuple[mol.Molecule, int]],products: list[tupl
 
 #h2 , o2 , h2o = mol.Molecule ("H2"), mol.Molecule ("O2"), mol.Molecule ("H2O")
 #print( valid_reaction ( reactives =[( h2 , 2), (o2 , 1)] , products =[( h2o , 2)]))
+
+def kinetic_decomp(A0: float, k: float, T: float, steps: int = 10, figure_path: str = None):
+    x=np.linspace(0,10,10)
+    y=[A0*2.7182818**(-k*t) for t in x]
+    if figure_path != None:
+        plt.plot(x,y)
+        plt.title("Evolution de [A](t)")
+        plt.xlabel("Temps (s)")
+        plt.ylabel("[A](t) (mol/L)")
+        plt.savefig(figure_path)
+        plt.show()
+    return y
+
+A_t = kinetic_decomp (A0 =0.1 , k=0.5 , T=5, steps =10)
+print (A_t )
+
+A_t = kinetic_decomp (A0 =0.1 , k=0.5 , T=5, steps =10 , figure_path ="test.jpeg")
