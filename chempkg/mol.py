221c7bcfcfb718389d79fd1513c75d99c05d5f47
lumy10 <bouabidnour02@gmail.com>
2025-12-12 11:46:32 +0100

    simplification parse formula

--- chempkg/mol.py
+++ chempkg/mol.py
@@ -5,39 +5,26 @@ from chempkg import atom
 biosphere_elements = {i.name:i for i in vars(atom).values() if isinstance(i, atom.Atom)}
 
 def parse_formula(s):
-    s=list(s)
-    i = 1
-    while i < len(s):
-        if s[i].isupper() and not s[i-1].isdigit():
-            s.insert(i, "1")
-            i += 1  # Sauter le "1" qu'on vient d'insérer
-        i += 1
-
-    # Si la formule finit par une lettre sans nombre ajouter 1
-    if s[-1].isalpha():
-        s += "1"
-        
-
-    #créer le dictionnaires, les clés sont les instances d'Atom, les valeurs sont les quantités
     d = {}
     i = 0
     while i<len(s):
-        if s[i].isalpha(): 
-            elem = s[i] 
-            if elem not in biosphere_elements:
-                raise ValueError(f"Élément chimique inconnu: {elem}")
-            i+=1
-            if s[i].islower():
-                elem += s[i]; i+=1
-            atom_instance = biosphere_elements[elem]
-            num = s[i]
-            while i < len(s)-1 and s[i+1].isdigit():
-                i += 1
-                num += s[i]
-            count = int(num)
-            d[atom_instance] = d.get(atom_instance, 0) + count
-
+        elem = s[i] 
+        if elem not in biosphere_elements:
+            raise ValueError(f"Élément chimique inconnu: {elem}")
+        #recuperer toutes les lettres du symbole
         i+=1
+        if i < len(s) and s[i].islower():
+            elem += s[i]; i+=1
+        atom_instance = biosphere_elements[elem]
+        #recuperer tout les chiffres de la quantité
+        num = ""
+        while i < len(s) and s[i].isdigit():
+            num += s[i]
+            i += 1
+        count = int(num) if num else 1
+        #créer le dictionnaires, les clés sont les instances d'Atom, les valeurs sont leurs quantités
+        d[atom_instance] = d.get(atom_instance, 0) + count
+
     return d
 
 class Molecule:
